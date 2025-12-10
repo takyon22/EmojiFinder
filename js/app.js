6d54b191b24c8f6c5c922e6ca48e1a143462b668
axel nell <6cne@proton.me>
2025-12-10 16:52:50 +0100

    kommentarer och justeringar på linux pc

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
+//const som länkar element i HTML/CSS (knappar, inputs, output divs) till denna JS filen
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 const alert = document.getElementById('alert');
 
+//function för emoji sök
 async function emojiSearch() {
+  
   const query = input.value.trim();
   if (!query) return;
 
-  const res = await fetch(`https://emojihub.yurace.pro/api/similar/${query}`); //Söker API med användarens input
+  const res = await fetch(`https://emojihub.yurace.pro/api/similar/${query}`); //Söker API med användarens input (query)
   const data = await res.json();
 
-  output.innerHTML = ""; // Rensa
+  output.innerHTML = ""; // Rensa tidigare sökning
 
   if (!data.length) {
     output.innerHTML = "<p>Inga resultat... 😔<p>";
     return;
   }
-  // Om inga träffar ^
+  // om inga träffar ^
 
   data.forEach(item => {
 
@@ -25,29 +28,30 @@ async function emojiSearch() {
     div.className = "emojis";
 
     div.innerHTML = `
-      <div class="emoji">${item.htmlCode}</div>
+      <div class="emoji">${item.htmlCode[0]}</div>
     `;
-    // Loopar igenom allt i array och levererar items i en div.
+    // loopar igenom allt i array och levererar items i en div.
+    // hämtar enbart första koden från htmlCode Array '[0]' för att undvika konstiga emojis
+    // hudfärgs emojis renderas inte korrekt på sidan så om jag skriver [0] så visas bara själva emojin.
 
     div.querySelector('.emoji').addEventListener('click', (e) => {
       navigator.clipboard.writeText(e.target.textContent)
+    //kopierar den klickade emojin till clipboard. här funkar det inte att låta användaren kopiera själva htmlkoden
+    //då klistras bara själva koden in, inte emojin. textContent fungerar korrekt.
 
       const temp = document.createElement('span');
       temp.textContent = 'Kopierad!';
       alert.appendChild(temp);
-
-      setTimeout(() => alert.removeChild(temp), 1000); // tar bort efter 2s
+      //skapar en alert som signalerar att emojin kopierats till clipboard
+      setTimeout(() => alert.removeChild(temp), 1000); // försvinner efter 2s
     });
 
     output.appendChild(div);
-
-    setTimeout(() => {
-      div.classList.add('show');
-    });
+    //slutligen lägger till den skapade emoji div till output div
   });
 }
 
-
+//function för random emoji sök
 async function randomSearch() {
   const res = await fetch('https://emojihub.yurace.pro/api/random');
   const data = await res.json();
@@ -56,12 +60,9 @@ async function randomSearch() {
 
   output.innerHTML = `
     <div class="emoji">${data.htmlCode[0]}</div>
-  `
-  // Hämtar enbart första koden från htmlCode Array '[0]' eftersom det är random
-
-  setTimeout(() => {
-    output.classList.add('show');
-  });
+  `;
+  // Hämtar enbart första koden från htmlCode Array '[0]' för att undvika konstiga emojis
+  // hudfärgs emojis renderas inte korrekt på sidan så om jag skriver [0] så visas bara själva emojin.
 
   output.querySelector('.emoji').addEventListener('click', (e) => {
     navigator.clipboard.writeText(e.target.textContent)
@@ -72,13 +73,14 @@ async function randomSearch() {
 
 
     setTimeout(() => alert.removeChild(temp), 1000); // tar bort efter 2s
+    
+    //jag gjorde denna lite annorlunda, utan emojis div t.ex. Det verkar inte behövas om det bara är 1 emoji som genereras
   });
 
 }
 
-button1.addEventListener('click', emojiSearch); //Kör function emojiSearch vid knapptryck
-button2.addEventListener('click', randomSearch); //Kör function randomSearch vid knapptryck
+button1.addEventListener('click', emojiSearch); //Kör function emojiSearch när användaren klickar på 'Hitta emoji' (button1)
+button2.addEventListener('click', randomSearch); //Kör function emojiSearch när användaren klickar på 'Slumpa emoji' (button2)
 
 input.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') emojiSearch();
-}); //Detta gör så man kan trycka enter för att söka och inte behöva trycka på knappen
+  if (e.key === 'Enter') emojiSearch()}); //Kör emojiSearch direkt efter användaren tryckt enter
